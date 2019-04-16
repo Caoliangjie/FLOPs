f2d66ce060a6b71991ffadcc25b8cf56d2d7d7c4
Caoliangjie <846768143@qq.com>
2019-04-16 14:58:57 +0800

    有点新东西

---
+++ readandwritejson_activity.py
@@ -0,0 +1,30 @@
+import json
+import csv
+count = 0
+fi = 'activity_net.v1-3.min.json'
+fi2 = 'lack_activitynet_v1-3.json'
+new_dict = {}
+n2 = {}
+with open(fi,'r') as load_f:
+ with open('local.txt','r') as f:
+  with open(fi2,'w') as f1:
+   f = list(f)##把这个转成list来做比较快
+   for i in range(len(f)):
+      f[i] = f[i][:-1]
+   d1 = json.load(load_f)
+   #print(type(d1['database']))
+   for k,v in d1.items():
+      if k=='taxonomy':
+        new_dict.setdefault(k,v)
+      if k=='version':
+        new_dict.setdefault(k,v)
+      if k=='database':
+        for k1,v1 in d1['database'].items():
+          if k1 not in f:
+            count +=1
+            n2.setdefault(k1,v1)
+        new_dict.setdefault(k,n2)
+   json.dump(new_dict,f1)
+print(count)
+for k,v in new_dict.items():
+   print(k)
